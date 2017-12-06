45f8a501b41598f4f01d069b58ee9d0074677c35
Jen Homann <jenhomann@Jens-MBP.igs2.com>
2017-12-06 15:08:06 -0600

    basic animations app with random guy from rick & morty

--- animations/ViewController.swift
+++ animations/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var button: UIButton!
+    var counter = 1
+    var isAnimating = false
+    var timer = Timer()
+    
+    @objc func animate() {
+        var counterString = "\(counter)"
+        
+        if counter < 10 {
+            counterString = "0\(counter)"
+        }
+        
+        image.image = UIImage(named: "frame_\(counterString)_delay-0.1s.gif")
+        counter+=1
+        
+        if counter == 38 {
+            counter = 0
+        }
+    }
+    
+    @IBAction func playButton(_ sender: Any) {
+        if isAnimating {
+            isAnimating = false
+            timer.invalidate()
+            button.setTitle("Start Animation", for: [])
+        } else {
+            isAnimating = true
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.animate), userInfo: nil, repeats: true)
+            button.setTitle("Stop Animation", for: [])
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
